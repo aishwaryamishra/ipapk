4af85bb883792bcad3e54cac0aa711010f2757fa
omnarayan <omnarayan@robustest.com>
2020-05-16 04:31:48 +0530

    aded modd

--- parser.go
+++ parser.go
@@ -16,7 +16,7 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/DHowett/go-plist"
+	"howett.net/plist"
 	"github.com/andrianbdn/iospng"
 	"github.com/shogo82148/androidbinary"
 	"github.com/shogo82148/androidbinary/apk"
